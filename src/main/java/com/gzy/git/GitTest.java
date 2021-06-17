cb9569914bcfc23bd10fac1b18a12bfe39c3e1a7
gzy <739586314@qq.com>
2021-06-17 10:38:12 +0800

    "first commit"

---
+++ src/main/java/com/gzy/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.gzy.git;
+
+/**
+ * @ClassName GitTest
+ * @Description
+ * @Author 高泽宇
+ * @Date 2021/6/17 10:34 上午
+ * @Version
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
